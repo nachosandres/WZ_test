a35e96001d1e4f473badf83514e0e6969582b264
jhoss <hoss@phys.ethz.ch>
2015-03-31 09:32:14 +0200

    SUSY3L: template, display card and display script for automated analysis

---
+++ scripts/Sub_susy3l_Display.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SRS=( "SR00" "SR01" "SR02" "SR03" "SR04" "SR05" "SR06" "SR07" "SR08" "SR09" "SR10" "SR11" "SR12" "SR13" "SR14" "SR15" "SR16" "SR17" "SR18" "SR19" "SR20" "SR21" "SR22" "SR23" "SR24" "SR25" "SR26" "SR27" "SR28" "SR29" )
+
+OSSFPAIR=( "on" "off" )
+
+for sr in ${SRS[@]}; do
+
+    for pair in ${OSSFPAIR[@]}; do
+
+        sregion=$sr
+        pairstate=$pair
+
+        cp cards/susy3l_auto.C cards/tmpFiles/susy3l_${sregion}_${pairstate}.C
+        sed -i 's|SIGNALREGION|'$sregion'|' cards/tmpFiles/susy3l_${sregion}_${pairstate}.C
+        sed -i 's|PAIRSTATE|'$pairstate'|' cards/tmpFiles/susy3l_${sregion}_${pairstate}.C
+
+        root -l -b cards/tmpFiles/susy3l_${sregion}_${pairstate}.C
+
+    done
+done
